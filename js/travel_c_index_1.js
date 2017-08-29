ad9a0cefe821b1736921d3648611f94a3b0a8a12
mi75 <missile@ukr.net>
2017-08-29 16:35:46 +0300

    Initial commit

---
+++ js/travel_c_index_1.js
@@ -0,0 +1,68 @@
+function init() {
+  window.addEventListener('scroll', function(e) {
+    var mmenu;
+    var pmenu;
+    var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+      headHeight = document.getElementById('hh').offsetHeight,
+      mmenu = document.getElementById("mm");
+      pmenu = document.getElementById("pull");
+    if (distanceY > headHeight) {
+      mmenu.classList.add("menu-fix");
+      pmenu.classList.add("menu-fix");
+    } else {
+      if (mmenu.classList.contains("menu-fix") || pmenu.classList.contains("menu-fix")){
+        mmenu.classList.remove("menu-fix");
+        pmenu.classList.remove("menu-fix");
+      }
+    }
+  });
+}
+window.onload = init();
+
+$(function() {
+      var pull    = $('#pull');
+        menu    = $('.main-menu ul');
+        menuHeight  = menu.height();
+
+      $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+      });
+
+      $(window).resize(function(){
+            var w = $(window).width();
+            if(w > 500 && menu.is(':hidden')) {
+              menu.removeAttr('style');
+            }
+        });
+    })
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+  goToSlide(currentSlide+1);
+}
+ 
+function previousSlide() {
+  goToSlide(currentSlide-1);
+}
+ 
+function goToSlide(n) {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (n+slides.length)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
+
+// var next = document.getElementById('next');
+// var previous = document.getElementById('previous');
+ 
+// next.onclick = function() {
+//   pauseSlideshow();
+//   nextSlide();
+// }
+// previous.onclick = function() {
+//   pauseSlideshow();
+//   previousSlide();
+// }
