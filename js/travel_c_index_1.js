1304089c7813464464b323a3be5e610dc85cc608
mi75 <missile@ukr.net>
2017-10-29 17:38:30 +0200

    slider scrolling 2 times slower

--- js/travel_c_index_1.js
+++ js/travel_c_index_1.js
@@ -39,7 +39,7 @@ $(function() {
 
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+var slideInterval = setInterval(nextSlide,4000);
 
 function nextSlide() {
   goToSlide(currentSlide+1);
