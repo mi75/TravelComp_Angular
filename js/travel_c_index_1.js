423d556f703dd080f85362632726f6885a6e0a2b
mi75 <missile@ukr.net>
2017-11-18 21:14:30 +0200

    new slider

--- js/travel_c_index_1.js
+++ js/travel_c_index_1.js
@@ -20,79 +20,72 @@ function init() {
 window.onload = init();
 
 $(function() {
-      var pull    = $('#pull');
-        menu    = $('.main-menu ul');
-        menuHeight  = menu.height();
+  var pull    = $('#pull');
+  menu    = $('.main-menu ul');
+  menuHeight  = menu.height();
 
-      $(pull).on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
-      });
+  $(pull).on('click', function(e) {
+    e.preventDefault();
+    menu.slideToggle();
+  });
 
-      $(window).resize(function(){
-            var w = $(window).width();
-            if(w > 500 && menu.is(':hidden')) {
-              menu.removeAttr('style');
-            }
-        });
-    })
-
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,4000);
-
-function nextSlide() {
-  goToSlide(currentSlide+1);
-}
- 
-function previousSlide() {
-  goToSlide(currentSlide-1);
-}
- 
-function goToSlide(n) {
-  slides[currentSlide].className = 'slide';
-  currentSlide = (n+slides.length)%slides.length;
-  slides[currentSlide].className = 'slide showing';
-}
-
-function slideNav() {
-  var parentB = document.getElementById('sl-k');
-  for (i = 0; i < slides.length; i++) {
-    var b = document.createElement('button');
-    b.innerHTML = '&nbsp';
-    b.className = "sl-b";
-    b.style.margin = '8px';
-    b.style.backgroundColor = 'transparent';
-    b.style.width = '11px';
-    b.style.height = '15px';
-    b.style.border = '2px solid';
-    b.style.borderColor = 'gray';
-    b.style.borderRadius = '50%';
-    b.style.cursor = 'pointer';
-    b.style.outline = 'none';
-    parentB.appendChild(b);
-  }
-  var knobs = document.getElementsByClassName('sl-b');
-  for (i = 0; i < knobs.length; i++) {
-    var a = function() {
-      alert(i);
+  $(window).resize(function(){
+    var w = $(window).width();
+    if(w > 500 && menu.is(':hidden')) {
+      menu.removeAttr('style');
     }
-    knobs[i].onclick = function() {
-      a();
+  });
+})
+
+window.requestAnimationFrame = (function () { // для поддержки requestAnimationFrame всеми браузерами
+        return window.requestAnimationFrame ||
+               function (callback) {
+                   return window.setTimeout(callback, 1000 / 60);
+               };
+})();
+// функция слайдера
+function slider(f, img, button, V, Vo) {
+  var iii = 0,
+      start = null,
+      clear = 0;
+  function step(time) {
+    if (start === null) start = time;
+    var progress = time - start;
+    if (progress > V) {
+      start = null;
+      for (var i=0; i<img.length; i++) {
+        img[i].style.zIndex = "0";
+        button[i].style.opacity = "1";
+      }
+      img[iii].style.zIndex = "1";
+      iii = ((iii != (img.length - 1)) ? (iii + 1) : 0);
+      img[iii].style.zIndex = "2";
+      img[iii].style.opacity = "0";
+      button[iii].style.opacity = ".4";
+    } else if(img[iii].style.opacity != "") {
+      img[iii].style.opacity = ((progress/Vo < 1) ? (progress/Vo) : 1);
+    }
+    if(clear != "0" && progress > Vo) {} else {requestAnimationFrame(step);}
+  }
+  requestAnimationFrame(step);
+  // f.onmouseenter = function() { if(clear == "0") clear = "1"; }  // при наведении на слайдер
+  // f.onmouseleave = function() { if(clear == "1") {clear = "0"; requestAnimationFrame(step);} }  // курсор убран со слайдера
+  for (var j=0; j<button.length; j++) {  // при нажатии кнопок
+    button[j].onclick = function() {
+      clear = "2";
+      for (var i=0; i<img.length; i++) {
+        img[i].style.zIndex = "0";
+      }
+      img[this.value].style.zIndex = "2";
+      img[this.value].style.opacity = "1";
+      for (var k=0; k<button.length; k++) {
+        button[k].style.opacity = ((button[k] == this) ? '.4' : '1');
+      }
     }
   }
-
 }
-window.onload = slideNav();
-
-// var next = document.getElementById('next');
-// var previous = document.getElementById('previous');
- 
-// next.onclick = function() {
-//   pauseSlideshow();
-//   nextSlide();
-// }
-// previous.onclick = function() {
-//   pauseSlideshow();
-//   previousSlide();
-// }
+// вызов функции слайдера
+var f = document.getElementById('m-sl'),
+    img = f.getElementsByClassName('slide'),
+    button = f.getElementsByTagName('div')[0].getElementsByTagName('button');
+slider(f, img, button, '4000', '200');  // '4000' — скорость смены картинок, '1000' — скорость прозрачности
