56bd3c07c90467c8f1c18078b854b6d88976c636
mi75 <missile@ukr.net>
2017-11-28 20:44:48 +0200

    add some vaidation functions

--- js/travel_c_contact_page.js
+++ js/travel_c_contact_page.js
@@ -37,3 +37,48 @@ $(function() {
         });
     })
 
+
+function showError(container, errorMessage) {
+      container.className = 'error';
+      var msgElem = document.createElement('span');
+      msgElem.className = "error-message";
+      msgElem.innerHTML = errorMessage;
+      container.appendChild(msgElem);
+    }
+
+    function resetError(container) {
+      container.className = '';
+      if (container.lastChild.className == "error-message") {
+        container.removeChild(container.lastChild);
+      }
+    }
+
+function validate(form) {
+      var elems = form.elements;
+
+      resetError(elems.from.parentNode);
+      if (!elems.from.value) {
+        showError(elems.from.parentNode, ' Укажите от кого.');
+      }
+
+      resetError(elems.mail.parentNode);
+      if (!elems.mail.value) {
+        showError(elems.mail.parentNode, ' Укажите адрес.');
+      }
+
+      resetError(elems.phone.parentNode);
+      if (!elems.phone.value) {
+        showError(elems.phone.parentNode, ' Укажите телефон.');
+      }
+
+      resetError(elems.how.parentNode);
+      if (!elems.how.value) {
+        showError(elems.how.parentNode, ' Укажите, откуда.');
+      }
+
+      resetError(elems.message.parentNode);
+      if (!elems.message.value) {
+        showError(elems.message.parentNode, ' Отсутствует текст.');
+      }
+
+    }
