00c4812b3a3bd41d1e68f04f527ead76e3f92833
mi75 <missile@ukr.net>
2017-12-06 17:40:01 +0200

    add "404 Not found" check

--- application.js
+++ application.js
@@ -3,83 +3,79 @@ var fs = require('fs');
 var qs = require('querystring');
 
 //create a server object:
-http.createServer(function (req, res) {
-var headers = {'Test-Header': 'Test'};
+http.createServer(function(req, res) {
+    var headers = { 'Test-Header': 'Test' };
 
-  try {
-	console.log(req.method);
-	console.log(req.url);
+    try {
+        console.log(req.method);
+        console.log(req.url);
 
-  	if (req.method == 'GET'){
- 		processGetRequest(req, res);
-  	} else {
-  		if (req.method == 'POST') {
-  			var body = '';
-  			var postData;
+        if (req.method == 'GET') {
+            processGetRequest(req, res);
+        } else {
+            if (req.method == 'POST') {
+                var body = '';
+                var postData;
 
-  			req.on('data', function (data) {
-            	body += data;
-    		});
+                req.on('data', function(data) {
+                    body += data;
+                });
 
-    		req.on('end', function () {
-            	postData = qs.parse(body);
-  			
-	  			if (req.url == '/api/contacts') {
-	  				console.log(postData);
+                req.on('end', function() {
+                    postData = qs.parse(body);
 
-	  				res.writeHead(301,
-					  {Location: '/contacts'}
-					);
-					res.end();
-	  			}
-        	});
+                    if (req.url == '/api/contacts') {
+                        console.log(postData);
 
-  		}
-  	}
-
-  }
-
-  catch (err){
-  	res.writeHead(501, headers);
-  	res.write('Exception occured');
-  	res.end();
-  	throw err;
-  }
+                        res.writeHead(301, { Location: '/contacts' });
+                        res.end();
+                    }
+                });
+            }
+        }
+    } catch (err) {
+        res.writeHead(501, headers);
+        res.write('Exception occured');
+        res.end();
+        throw err;
+    }
 
 }).listen(8080);
 
-function processGetRequest(req, res){
+function processGetRequest(req, res) {
 
-  	var targetFileName;
+    var targetFileName;
+    switch (req.url) {
+        case '/':
+            {
+                targetFileName = __dirname + '/index.html';
+                break;
+            }
+        case '/contacts':
+            {
+                targetFileName = __dirname + '/contact_page.html';
+                break;
+            }
+        case '/favicon.ico':
+            {
+                targetFileName = __dirname + '/img/t.png';
+                break;
+            }
+        default:
+            {
+                targetFileName = __dirname + req.url;
+            }
+    }
 
-	switch(req.url){
-	  		case '/': {
-				targetFileName = __dirname + '/index.html';
-				break;
-	  		}
-	  		case '/contacts': {
-				targetFileName = __dirname + '/contact_page.html';
-				break;
-	  		}
-	  		case '/favicon.ico': {
-				targetFileName = __dirname + '/img/t.png';
-				break;
-	  		}
-	  		default :{
-	  			targetFileName = __dirname + req.url;
-	  		}
-	}
-	  		
-	if (fs.existsSync(targetFileName)) {
-	  	fs.readFile(targetFileName, function(err, data) {
-	    	res.writeHead(200, {});
-	    	res.write(data);
-	    	res.end();
-		});
-	}
-	else {
-		res.writeHead(404);
-		res.write('Not found');
-		res.end();
-	}
+    if (fs.existsSync(targetFileName)) {
+        fs.readFile(targetFileName, function(err, data) {
+            res.writeHead(200, {});
+            res.write(data);
+            res.end();
+        });
+    } else {
+        res.writeHead(404);
+        res.write('Not found');
+        res.end();
+    }
 }
