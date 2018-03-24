f20fad9aa7fb0c07c729a8fdda792151a6528725
mi75 <missile@ukr.net>
2018-03-24 21:03:24 +0200

    universalized servise

--- src/app/_services/m-reviews.service.ts
+++ src/app/_services/m-reviews.service.ts
@@ -5,10 +5,10 @@ export class MReviewsService {
 
   constructor() { }
 
-  checkPhoto(arr, arrLen){
+  checkExist(arr, arrLen, ifEmpty){
     for (let i=0; i<arrLen; i++){
       if (!arr[i].photo) {
-        arr[i].photo = 'default_customer.jpg';
+        arr[i].photo = ifEmpty;
       }
     }
   }
