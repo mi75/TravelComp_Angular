a66d44403786709d9492a475f8ce54f1e8124f64
mi75 <missile@ukr.net>
2018-03-24 19:46:59 +0200

    photo and date corrections

--- src/app/m-reviews/m-reviews.component.ts
+++ src/app/m-reviews/m-reviews.component.ts
@@ -10,7 +10,7 @@ import { ApiGetCallerService } from '../_services/api-get-caller.service';
 export class MReviewsComponent implements OnInit {
 
   feedbacks;
-  @Input() usersImgPath:string = "D:/Work/tmp/P3TravelComp/upload/";
+  @Input() usersImgPath:string = "/assets/images/upload/";
   // @Output() feedbacksForwarding = new EventEmitter();
   startRow=0;
   rowsCounter = 0;
@@ -24,6 +24,11 @@ export class MReviewsComponent implements OnInit {
     this.load.getData('http://127.0.0.1:8080/api/feedback?startRow=0').subscribe( res => {
       this.feedbacks = res.rows;
       this.rowsCounter = res.count;
+      for (let i=0; i<this.feedbacks.length; i++){
+        if (!this.feedbacks[i].photo) {
+          this.feedbacks[i].photo = 'default_customer.jpg';
+        }
+      }
     } )
   }
 
@@ -32,6 +37,11 @@ export class MReviewsComponent implements OnInit {
     if (this.startRow > this.rowsCounter - 3) this.startRow = this.rowsCounter - 3;
     this.load.getData('http://127.0.0.1:8080/api/feedback?startRow='+this.startRow).subscribe( res => {
       this.feedbacks = res.rows;
+      for (let i=0; i<this.feedbacks.length; i++){
+        if (!this.feedbacks[i].photo) {
+          this.feedbacks[i].photo = 'default_customer.jpg';
+        }
+      }
     } )
   }
 
@@ -40,6 +50,11 @@ export class MReviewsComponent implements OnInit {
     if (this.startRow < 0) this.startRow = 0;
     this.load.getData('http://127.0.0.1:8080/api/feedback?startRow='+this.startRow).subscribe( res => {
       this.feedbacks = res.rows;
+      for (let i=0; i<this.feedbacks.length; i++){
+        if (!this.feedbacks[i].photo) {
+          this.feedbacks[i].photo = 'default_customer.jpg';
+        }
+      }
     } )
   }
 
