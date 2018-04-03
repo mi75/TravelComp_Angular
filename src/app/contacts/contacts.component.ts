71d6ce12697f54566cbf8d1d0dcf665ba9e29f83
mi75 <missile@ukr.net>
2018-04-03 18:53:47 +0300

    some reset contact form

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ValidatorService } from '../_services/validator.service';
+// import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'contacts',
@@ -15,6 +16,8 @@ export class ContactsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  // form: FormGroup;
 
   resetError(elem) {
     elem.parentNode.className = 'f-row';
@@ -32,60 +35,77 @@ export class ContactsComponent implements OnInit {
 }
 
   toValidate(newMessage, newName, newEmail, newPhone, newHowHeard, newKeepMe){
+    let er = null;
 
     this.resetError(newMessage);
     if (!this.valid.validator.validate(newMessage.value, this.valid.validator.rules.notEmpty)) {
         this.showError(newMessage, ' Отсутствует текст.');
-        // er = 1;
+        er = 1;
     } else {
         if (!this.valid.validator.validate(newMessage.value, this.valid.validator.rules.charsCount, 1000)) {
             this.showError(newMessage, ' максимум 1000 символов.');
-            // er = 1;
+            er = 1;
         }
     }
 
     this.resetError(newName);
     if (!this.valid.validator.validate(newName.value, this.valid.validator.rules.notEmpty)) {
         this.showError(newName, ' Укажите, от кого.');
-        // er = 1;
+        er = 1;
     } else {
         if (!this.valid.validator.validate(newName.value, this.valid.validator.rules.clientName)) {
           this.resetError(newName);
           this.showError(newName, ' допустимы только буквы.');
-            // er = 1;
+            er = 1;
         }
     }
     if (!this.valid.validator.validate(newName.value, this.valid.validator.rules.charsCount, 80)) {
       this.showError(newName, ' максимум 80 символов.');
-        // er = 1;
+        er = 1;
     }
 
     this.resetError(newEmail);
     if (!newEmail.value) {
       this.showError(newEmail, ' Укажите адрес.');
-        // er = 1;
+        er = 1;
     } else {
         if (!this.valid.validator.validate(newEmail.value, this.valid.validator.rules.email)) {
           this.showError(newEmail, ' некорректный адрес');
-            // er = 1;
+            er = 1;
         }
     }
 
     this.resetError(newPhone);
     if (!this.valid.validator.validate(newPhone.value, this.valid.validator.rules.notEmpty)) {
       this.showError(newPhone, ' Укажите телефон.');
-        // er = 1;
+        er = 1;
     } else {
         if (!this.valid.validator.validate(newPhone.value, this.valid.validator.rules.phone)) {
           this.showError(newPhone, ' некорректный номер');
-            // er = 1;
+            er = 1;
         }
     }
 
     this.resetError(newHowHeard);
     if (!newHowHeard.value) {
         this.showError(newHowHeard, ' Укажите источник.');
-        // er = 1;
+        er = 1;
     }
+
+    if (!er) {
+      this.sendMySuggestion();
+      newMessage.value = '';
+      newName.value = '';
+      newEmail.value = '';
+      newPhone.value = '';
+      newHowHeard.value = '';
+      newKeepMe.value = 'on';
+    }
+  }
+
+  sendMySuggestion() {
+    // this.form.reset();
+    // this.form.reset(this.form.value);
+    // sendPost(uploadAddress, formData, function() { document.forms.mySuggestion.reset(); }, function(errorMessage) { alert(errorMessage) }); //from apiCaller.js
   }
 }
