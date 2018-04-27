bbc619820ec2bd9e14b1653a1aa590117d2e0bc2
mi75 <missile@ukr.net>
2018-04-27 19:49:24 +0300

    review form validate

--- src/app/modal-reviews-form/modal-reviews-form.component.ts
+++ src/app/modal-reviews-form/modal-reviews-form.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, AbstractControl } from '@angular/forms';
+import { ValidatorService } from '../_services/validator.service';
+import { ApiCallerService } from '../_services/api-caller.service';
 
 @Component({
   selector: 'modal-reviews-form',
   templateUrl: './modal-reviews-form.component.html',
-  styleUrls: ['./modal-reviews-form.component.css']
+  styleUrls: ['./modal-reviews-form.component.css'],
+  providers: [ValidatorService, ApiCallerService]
 })
 export class ModalReviewsFormComponent implements OnInit {
 
-  constructor() { }
+  public feedbackForm: FormGroup;
+
+  constructor(
+    private valid: ValidatorService,
+    private send: ApiCallerService,
+    private _fb: FormBuilder
+  ) {
+    this.feedbackForm = this._fb.group({
+      message: this._fb.control('', [
+        valid.messageValidator()
+      ]),
+      from: this._fb.control('', [
+        valid.userNameValidator()
+      ]),
+      photo: this._fb.control('')
+    });
+   }
 
   ngOnInit() {
   }
 
   public visible = false;
   public visibleAnimate = false;
+  // public ff: string = '';
 
   public show(): void {
     this.visible = true;
@@ -28,6 +49,26 @@ export class ModalReviewsFormComponent implements OnInit {
   public onContainerClicked(event: MouseEvent): void {
     if ((<HTMLElement>event.target).classList.contains('modal')) {
       this.hide();
+    }
+  }
+
+  sendMyReview(data: FormGroup): void {
+
+    Object.keys(this.feedbackForm.controls).forEach(field => { 
+      const control = this.feedbackForm.get(field); 
+      control.markAsTouched({ onlySelf: true });
+    });
+
+    if (this.feedbackForm.valid) {
+      this.send.postData('api/feedback', data)
+      .subscribe(
+        error => alert(error)
+      );
+      this.feedbackForm.reset({
+        message: '',
+        from: '',
+        photo: ''
+      });
     }
   }
 
